76b669ca6c4c4818ab4e4fc4713892083a84ff68
Alberto <Alberto@172.19.186.188>
2018-03-05 10:25:26 +0100

    Part A2 still not working

--- A2/Signalled_Queue.java
+++ A2/Signalled_Queue.java
@@ -2,30 +2,43 @@ package concurrent_assignment2.A2;
 
 import concurrent_assignment2.A_intro.Queue;
 
+
 /**
- * Use the synchronized keyword and signals so that
- * you do not need to busy wait.
- * But of course you still need your variable to know
- * whose's turn it is.
+ * Use the synchronized keyword and signals so that you do not need to busy
+ * wait. But of course you still need your variable to know whose's turn it is.
  *
  */
- 
-class Signalled_Queue implements Queue{
-	int n=0;
-	
-	@Override
-	public void read() {
-		// TODO Auto-generated method stub
-		
-	}
+class Signalled_Queue implements Queue {
 
-	@Override
-	public void write(int x) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
+    int n = 0;
+    boolean readerturn = false;
+
+    @Override
+    synchronized public void read() {
+        if (!readerturn) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+        }
+        System.out.println(n+" reader");
+        notify();
+
+    }
+
+    @Override
+    synchronized public void write(int x) {
+        if(readerturn){
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+        }
+        n=x;
+        System.out.println(n+" writer");
+        notify();
+    }
+
 }
-
-
