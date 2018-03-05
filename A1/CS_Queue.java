a3c465a8610c194dad5f88c1cd4cab00de3f862b
usuario <usuario@161.67.196.143>
2018-03-05 09:29:44 +0100

    Part A1 finished

--- A1/CS_Queue.java
+++ A1/CS_Queue.java
@@ -10,19 +10,28 @@ import concurrent_assignment2.A_intro.Queue;
  * Set a meaningful name for such variable.
  * */
  
-class CS_Queue implements Queue{
+class CS_Queue implements Queue {
 	int n=0;
-	
+	volatile boolean readerturn;
+        
 	@Override
 	public void read() {
 		// TODO Auto-generated method stub
+                while (!readerturn);
+                    
+                System.out.println(n+" reader");
+                readerturn=false;
 		
 	}
 
 	@Override
 	public void write(int x) {
 		// TODO Auto-generated method stub
-		
+                while (readerturn);
+                n=x;
+                
+                 System.out.println(n);
+                 readerturn=true;
 	}
 	
 	
