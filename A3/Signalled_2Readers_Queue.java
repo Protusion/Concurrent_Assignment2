7af9c78fa38379e1aac00e5e8378917c48e820fb
Alberto <Alberto@172.19.186.188>
2018-03-12 17:11:24 +0100

    Part A3 finished

--- A3/Signalled_2Readers_Queue.java
+++ A3/Signalled_2Readers_Queue.java
@@ -4,32 +4,51 @@ import concurrent_assignment2.A_intro.Queue;
 
 /**
  * Use the synchronized keyword and signals.
- * 
- * You cannot decide whose's turn it is based on 
- * a 2 states variables, so know use a variable which
- * allows for more values (you need 3 states, that is, 3 turns).
+ *
+ * You cannot decide whose's turn it is based on a 2 states variables, so know
+ * use a variable which allows for more values (you need 3 states, that is, 3
+ * turns).
  *
  */
- 
-class Signalled_2Readers_Queue implements Queue{
-	int n=0;
-	
-	@Override
-	synchronized public void read() {
-				
-	}
-        
-        @Override
-        synchronized public void read(int ID){
-            
+class Signalled_2Readers_Queue implements Queue {
+
+    int n = 0;
+    int turn = 0;
+
+    @Override
+    synchronized public void read() {
+
+    }
+
+    @Override
+    synchronized public void read(int ID) {
+        while (turn != ID) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
         }
 
-	@Override
-	synchronized public void write(int x) {
-            
-	}
-	
-	
+        System.out.println(n + " Reader");
+        turn++;
+        notifyAll();
+
+    }
+
+    @Override
+    synchronized public void write(int x) {
+        while (turn != 2) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                System.out.println(ex);
+            }
+        }
+        n = x;
+        System.out.println(n);
+        turn = 0;
+        notifyAll();
+    }
+
 }
-
-
