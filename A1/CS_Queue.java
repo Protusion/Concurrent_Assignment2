57d1510ebd64079047c5c668bbd2f1d52147d251
Alberto <Alberto@172.19.186.188>
2018-03-12 09:36:17 +0100

    Part A1 finished

--- A1/CS_Queue.java
+++ A1/CS_Queue.java
@@ -12,13 +12,16 @@ import concurrent_assignment2.A_intro.Queue;
  
 class CS_Queue implements Queue {
 	int n=0;
-	volatile boolean readerturn;
+	boolean readerturn;
         
 	@Override
 	public void read() {
-		// TODO Auto-generated method stub
-                while (!readerturn);
-                    
+		
+	}
+        
+        @Override
+	public void read(int ID) {
+                while (!readerturn){}
                 System.out.println(n+" reader");
                 readerturn=false;
 		
@@ -26,15 +29,11 @@ class CS_Queue implements Queue {
 
 	@Override
 	public void write(int x) {
-		// TODO Auto-generated method stub
-                while (readerturn);
+                while (readerturn){}
                 n=x;
-                
-                 System.out.println(n);
-                 readerturn=true;
-	}
-	
-	
+                System.out.println(n+" writer");
+                readerturn=true;
+	}	
 }
 
 
--- A1/Main.java
+++ A1/Main.java
@@ -11,16 +11,15 @@ public class Main {
 		CS_Queue queue;
 		
 		queue=new CS_Queue();
-		
-		new Writer(queue);
+                new Writer(queue);
 		new Reader(queue);
 	
 
 		
 		//cola=new ColaSynchronized();
 		//cola=new ColaBusyWait();
-		//cola=new ColaConSeñales();
-		//queue=new ColaConSeñalesVariosConsumidores();
+		//cola=new ColaConSeï¿½ales();
+		//queue=new ColaConSeï¿½alesVariosConsumidores();
 		//cola=new ColaActivaVariosConsumidores();
 		
 		//new Consumidor(cola);
