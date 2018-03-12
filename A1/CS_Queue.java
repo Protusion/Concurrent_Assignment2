9a7e10c3a602f328759c8879fb99c3892f120163
Alberto <Alberto@172.19.186.188>
2018-03-12 17:10:42 +0100

    Part A1 finished (fixed reader with parameter)

--- A1/CS_Queue.java
+++ A1/CS_Queue.java
@@ -2,38 +2,38 @@ package concurrent_assignment2.A1;
 
 import concurrent_assignment2.A_intro.Queue;
 
-/**Use condition synchronization by means of busy wait.
- * 
- * What kind of variable do you need to implement busy
- * wait synchronization?
- * 
+/**
+ * Use condition synchronization by means of busy wait.
+ *
+ * What kind of variable do you need to implement busy wait synchronization?
+ *
  * Set a meaningful name for such variable.
- * */
- 
+ *
+ */
 class CS_Queue implements Queue {
-	int n=0;
-	boolean readerturn;
-        
-	@Override
-	public void read() {
-		
-	}
-        
-        @Override
-	public void read(int ID) {
-                while (!readerturn){}
-                System.out.println(n+" reader");
-                readerturn=false;
-		
-	}
 
-	@Override
-	public void write(int x) {
-                while (readerturn){}
-                n=x;
-                System.out.println(n+" writer");
-                readerturn=true;
-	}	
+    int n = 0;
+    boolean readerturn;
+
+    @Override
+    public void read() {
+        while (!readerturn) {
+        }
+        System.out.println(n + " reader");
+        readerturn = false;
+    }
+
+    @Override
+    public void read(int ID) {
+        //never used
+    }
+
+    @Override
+    public void write(int x) {
+        while (readerturn) {
+        }
+        n = x;
+        System.out.println(n + " writer");
+        readerturn = true;
+    }
 }
-
-
